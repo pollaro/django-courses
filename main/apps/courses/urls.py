145144fd5f6067f1388713dace052ae5ccde77ee
Jim <jim.pollaro@gmail.com>
2017-09-20 16:51:40 -0700

    Done with mandatory parts

--- main/apps/courses/urls.py
+++ main/apps/courses/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.index,name='index'),
-    url(r'^(?P<id>[0-9]{1,})/destroy',views.destroy,name='destroy')
+    url(r'^(?P<id>[0-9]{1,})/destroy',views.destroy,name='destroy'),
+    url(r'^addCourse',views.addCourse,name='addCourse'),
+    url(r'^(?P<id>[0-9]{1,})/confirm',views.confirm,name='confirm')
 ]
--- main/apps/courses/views.py
+++ main/apps/courses/views.py
@@ -2,7 +2,22 @@ from django.shortcuts import render, redirect
 from .models import Courses
 
 def index(request):
-    return render(request,'courses/index.html')
+    context = {
+        'courses':Courses.objects.all()
+    }
+    return render(request,'courses/index.html',context)
 
 def destroy(request,id):
+    course = {
+        'course':Courses.objects.get(id=id)
+    }
+    return render(request,'courses/destroy.html',course)
+
+def addCourse(request):
+    Courses.objects.create(name=request.POST['courseName'],desc=request.POST['descField'])
+    return redirect(index)
+
+def confirm(request,id):
+    c = Courses.objects.get(id=id)
+    c.delete()
     return redirect(index)
